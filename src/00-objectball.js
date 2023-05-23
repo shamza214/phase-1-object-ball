c459e96365e4a93515e53e490960342e92cdbb65
Syed Hamza <syedr.hamza@gmail.com>
2023-05-23 15:15:19 -0400

    first commit

---
+++ src/00-objectball.js
@@ -0,0 +1,19 @@
+function gameObject() {
+    return { 
+    teamName: `Brooklyn Nets`,
+    colors: [black, white],
+    players: {
+        playerName:`Alan Anderson`, 
+        number: 0, 
+        shoe: 16,
+        points: 22,
+        rebounds: 12,
+        assists: 12,
+        steals: 3,
+        blocks: 1,
+        slamDunks: 1,
+        
+    }
+    }
+}
+//console.log(gameObject)
